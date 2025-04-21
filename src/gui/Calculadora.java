08ae2dbce27787e7c88e6277d0e56f09098db2ba
Martín Alejandro García <martinalejandrogarciababastro@gmail.com>
2025-04-20 21:21:09 -0400

    fix: fue eliminado la operacion "%" ,agregado los botones de parentesis e historial de operaciones

--- src/gui/Calculadora.java
+++ src/gui/Calculadora.java
@@ -1,5 +1,6 @@
-package gui;
+ package gui;
 
+ import javax.swing.*;
 import logica.Polinomio;
 import java.awt.Color;
 import java.awt.EventQueue;
@@ -7,7 +8,7 @@ import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.server.Operation;
-
+import init.Iniciadora;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -15,6 +16,8 @@ import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import net.miginfocom.swing.MigLayout;
+import javax.swing.JTextArea;
+import javax.swing.JScrollBar;
 
 public class Calculadora extends JFrame {
 
@@ -44,33 +47,36 @@ public class Calculadora extends JFrame {
 		setFont(new Font("Arial Black", Font.PLAIN, 12));
 		setForeground(Color.RED);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 285, 332);
+		setBounds(100, 100, 282, 437);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[67px][67px][68px][67px]", "[53px][46px][46px][46px][46px][46px]"));
+		contentPane.setLayout(null);
 
 		textField = new JTextField();
+		textField.setBounds(77, 12, 190, 49);
 		textField.setFont(new Font("Tahoma", Font.PLAIN, 18));
 		textField.setHorizontalAlignment(SwingConstants.RIGHT);
 		textField.setBackground(Color.GRAY);
 		textField.setForeground(Color.WHITE);
-		contentPane.add(textField, "cell 0 0 4 1,grow");
+		contentPane.add(textField);
 		textField.setColumns(10);
 		textField.setEditable(false);
-		JButton button = new JButton("%");
-		button.setFont(new Font("Tahoma", Font.PLAIN, 16));
-		button.setForeground(Color.WHITE);
-		button.setBackground(Color.DARK_GRAY);
-		button.addActionListener(new ActionListener() {
+		JButton parentesisIzq = new JButton("(");
+		parentesisIzq.setBounds(12, 65, 61, 42);
+		parentesisIzq.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		parentesisIzq.setForeground(Color.WHITE);
+		parentesisIzq.setBackground(Color.DARK_GRAY);
+		parentesisIzq.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 
 			}
 		});
-		button.setHorizontalTextPosition(SwingConstants.CENTER);
-		contentPane.add(button, "cell 0 1,grow");
+		parentesisIzq.setHorizontalTextPosition(SwingConstants.CENTER);
+		contentPane.add(parentesisIzq);
 
 		JButton btnCe = new JButton("CE");
+		btnCe.setBounds(12, 16, 60, 42);
 		btnCe.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnCe.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -85,9 +91,10 @@ public class Calculadora extends JFrame {
 		btnCe.setForeground(Color.WHITE);
 		btnCe.setBackground(Color.DARK_GRAY);
 		btnCe.setHorizontalTextPosition(SwingConstants.CENTER);
-		contentPane.add(btnCe, "cell 1 1,grow");
+		contentPane.add(btnCe);
 
 		JButton btnC = new JButton("C");
+		btnC.setBounds(141, 65, 62, 42);
 		btnC.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnC.setForeground(Color.WHITE);
 		btnC.setBackground(Color.DARK_GRAY);
@@ -99,9 +106,10 @@ public class Calculadora extends JFrame {
 				} catch(Exception e) {}
 			}
 		});
-		contentPane.add(btnC, "cell 2 1,grow");
+		contentPane.add(btnC);
 
 		JButton btnErase = new JButton("<--");
+		btnErase.setBounds(12, 249, 61, 42);
 		btnErase.setBackground(Color.BLACK);
 		btnErase.setForeground(Color.WHITE);
 		btnErase.setFont(new Font("Tahoma", Font.PLAIN, 16));
@@ -117,9 +125,10 @@ public class Calculadora extends JFrame {
 				} catch (Exception e) {}
 			}
 		});
-		contentPane.add(btnErase, "cell 0 5,grow");
+		contentPane.add(btnErase);
 
 		JButton btn7 = new JButton("7");
+		btn7.setBounds(12, 111, 61, 42);
 		btn7.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btn7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -131,9 +140,10 @@ public class Calculadora extends JFrame {
 		});
 		btn7.setBackground(Color.BLACK);
 		btn7.setForeground(Color.WHITE);
-		contentPane.add(btn7, "cell 0 2,grow");
+		contentPane.add(btn7);
 
 		JButton btn8 = new JButton("8");
+		btn8.setBounds(77, 111, 60, 42);
 		btn8.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btn8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -145,9 +155,10 @@ public class Calculadora extends JFrame {
 		});
 		btn8.setForeground(Color.WHITE);
 		btn8.setBackground(Color.BLACK);
-		contentPane.add(btn8, "cell 1 2,grow");
+		contentPane.add(btn8);
 
 		JButton btn9 = new JButton("9");
+		btn9.setBounds(141, 111, 62, 42);
 		btn9.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btn9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -159,9 +170,10 @@ public class Calculadora extends JFrame {
 		});
 		btn9.setForeground(Color.WHITE);
 		btn9.setBackground(Color.BLACK);
-		contentPane.add(btn9, "cell 2 2,grow");
+		contentPane.add(btn9);
 
 		JButton btn4 = new JButton("4");
+		btn4.setBounds(12, 157, 61, 42);
 		btn4.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btn4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -173,9 +185,10 @@ public class Calculadora extends JFrame {
 		});
 		btn4.setForeground(Color.WHITE);
 		btn4.setBackground(Color.BLACK);
-		contentPane.add(btn4, "cell 0 3,grow");
+		contentPane.add(btn4);
 
 		JButton btn5 = new JButton("5");
+		btn5.setBounds(77, 157, 60, 42);
 		btn5.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btn5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -187,9 +200,10 @@ public class Calculadora extends JFrame {
 		});
 		btn5.setForeground(Color.WHITE);
 		btn5.setBackground(Color.BLACK);
-		contentPane.add(btn5, "cell 1 3,grow");
+		contentPane.add(btn5);
 
 		JButton btn6 = new JButton("6");
+		btn6.setBounds(141, 157, 62, 42);
 		btn6.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btn6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -201,9 +215,10 @@ public class Calculadora extends JFrame {
 		});
 		btn6.setForeground(Color.WHITE);
 		btn6.setBackground(Color.BLACK);
-		contentPane.add(btn6, "cell 2 3,grow");
+		contentPane.add(btn6);
 
 		JButton btn1 = new JButton("1");
+		btn1.setBounds(12, 203, 61, 42);
 		btn1.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btn1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -215,9 +230,10 @@ public class Calculadora extends JFrame {
 		});
 		btn1.setForeground(Color.WHITE);
 		btn1.setBackground(Color.BLACK);
-		contentPane.add(btn1, "cell 0 4,grow");
+		contentPane.add(btn1);
 
 		JButton btn2 = new JButton("2");
+		btn2.setBounds(77, 203, 60, 42);
 		btn2.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btn2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -229,9 +245,10 @@ public class Calculadora extends JFrame {
 		});
 		btn2.setForeground(Color.WHITE);
 		btn2.setBackground(Color.BLACK);
-		contentPane.add(btn2, "cell 1 4,grow");
+		contentPane.add(btn2);
 
 		JButton btn3 = new JButton("3");
+		btn3.setBounds(141, 203, 62, 42);
 		btn3.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btn3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -243,9 +260,10 @@ public class Calculadora extends JFrame {
 		});
 		btn3.setForeground(Color.WHITE);
 		btn3.setBackground(Color.BLACK);
-		contentPane.add(btn3, "cell 2 4,grow");
+		contentPane.add(btn3);
 
 		JButton btnDiv = new JButton("/");
+		btnDiv.setBounds(207, 65, 60, 42);
 		btnDiv.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnDiv.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -257,9 +275,10 @@ public class Calculadora extends JFrame {
 		});
 		btnDiv.setBackground(Color.DARK_GRAY);
 		btnDiv.setForeground(Color.WHITE);
-		contentPane.add(btnDiv, "cell 3 1,grow");
+		contentPane.add(btnDiv);
 
 		JButton btnMult = new JButton("*");
+		btnMult.setBounds(207, 111, 60, 42);
 		btnMult.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnMult.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -271,9 +290,10 @@ public class Calculadora extends JFrame {
 		});
 		btnMult.setBackground(Color.DARK_GRAY);
 		btnMult.setForeground(Color.WHITE);
-		contentPane.add(btnMult, "cell 3 2,grow");
+		contentPane.add(btnMult);
 
 		JButton btnResta = new JButton("-");
+		btnResta.setBounds(207, 157, 60, 42);
 		btnResta.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnResta.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -285,9 +305,10 @@ public class Calculadora extends JFrame {
 		});
 		btnResta.setBackground(Color.DARK_GRAY);
 		btnResta.setForeground(Color.WHITE);
-		contentPane.add(btnResta, "cell 3 3,grow");
+		contentPane.add(btnResta);
 
 		JButton btnSuma = new JButton("+");
+		btnSuma.setBounds(207, 203, 60, 42);
 		btnSuma.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnSuma.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -299,9 +320,10 @@ public class Calculadora extends JFrame {
 		});
 		btnSuma.setBackground(Color.DARK_GRAY);
 		btnSuma.setForeground(Color.WHITE);
-		contentPane.add(btnSuma, "cell 3 4,grow");
+		contentPane.add(btnSuma);
 
 		JButton btn0 = new JButton("0");
+		btn0.setBounds(77, 249, 60, 42);
 		btn0.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btn0.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -313,9 +335,10 @@ public class Calculadora extends JFrame {
 		});
 		btn0.setForeground(Color.WHITE);
 		btn0.setBackground(Color.BLACK);
-		contentPane.add(btn0, "cell 1 5,grow");
+		contentPane.add(btn0);
 
 		JButton btnPunto = new JButton(".");
+		btnPunto.setBounds(141, 249, 62, 42);
 		btnPunto.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnPunto.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -327,9 +350,10 @@ public class Calculadora extends JFrame {
 		});
 		btnPunto.setForeground(Color.WHITE);
 		btnPunto.setBackground(Color.BLACK);
-		contentPane.add(btnPunto, "cell 2 5,grow");
+		contentPane.add(btnPunto);
 
 		JButton btnIgual = new JButton("=");
+		btnIgual.setBounds(207, 249, 60, 42);
 		btnIgual.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btnIgual.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -341,6 +365,39 @@ public class Calculadora extends JFrame {
 		});
 		btnIgual.setBackground(Color.RED);
 		btnIgual.setForeground(Color.WHITE);
-		contentPane.add(btnIgual, "cell 3 5,grow");
+		contentPane.add(btnIgual);
+		
+		JButton button = new JButton(")");
+		button.setBounds(76, 65, 61, 42);
+		button.setBackground(Color.DARK_GRAY);
+		button.setForeground(Color.WHITE);
+		contentPane.add(button);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(12, 322, 255, 73);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(10, 5, 218, 77);
+		panel.add(textArea);
+		textArea.setEditable(false);
+		final JScrollBar scrollBar = new JScrollBar();
+		scrollBar.setBounds(228, 5, 17, 77);
+		panel.add(scrollBar);
+		
+		JButton btnSh = new JButton("H");
+		btnSh.setBackground(Color.DARK_GRAY);
+		btnSh.setForeground(Color.WHITE);
+		btnSh.setSelectedIcon(new ImageIcon(Calculadora.class.getResource("/com/sun/javafx/scene/control/skin/caspian/dialog-more-details@2x.png")));
+		btnSh.setBounds(12, 294, 61, 23);
+		contentPane.add(btnSh);
+		 btnSh.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                scrollBar.setVisible(!scrollBar.isVisible()); // Alternar visibilidad
+	                revalidate(); // Actualizar el layout
+	            }
+	        });
 	}
 }
